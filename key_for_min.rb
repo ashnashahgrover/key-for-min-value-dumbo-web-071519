d52d4acbc7192f926f08044a746cdc62762aac29
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 01:22:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
-
-[25, 45, 56]
-
-
-i = 0
-z = 1
-
-for z in 1:array.size 
-  if array[i] < array [z]
-    i  
-  else 
-    i = z
  
 
 def key_for_min_value(name_hash)
